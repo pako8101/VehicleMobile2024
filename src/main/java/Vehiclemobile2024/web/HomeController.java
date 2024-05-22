3bfa4b4f8ee16e8f43bec4b3f5b9effd1c26588c
pako8101 <pako810129@gmail.com>
2024-05-22 08:05:43 +0300

    initial commit

--- src/main/java/Vehiclemobile2024/web/HomeController.java
+++ src/main/java/Vehiclemobile2024/web/HomeController.java
@@ -1,2 +1,15 @@
-package Vehiclemobile2024.web;public class HomeController {
+package Vehiclemobile2024.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+
+        return "index";
+    }
+
 }
